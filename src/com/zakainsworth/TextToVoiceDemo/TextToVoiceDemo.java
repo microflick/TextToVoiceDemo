b6553bc2b1a9e1d28096b848178efcc611b98de1
Zak Ainsworth <zakainsworth@gmail.com>
2010-07-20 01:10:12 -0400

    First commit.

---
+++ src/com/zakainsworth/TextToVoiceDemo/TextToVoiceDemo.java
@@ -0,0 +1,56 @@
+package com.zakainsworth.TextToVoiceDemo;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.media.MediaPlayer;
+
+public class TextToVoiceDemo extends Activity {
+
+	private Button speakBtn;
+	private EditText inputText;
+    private MediaPlayer mPlayer = new MediaPlayer();
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        inputText = (EditText) findViewById(R.id.inputText);
+        speakBtn = (Button) findViewById(R.id.speakBtn);
+        
+        speakBtn.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		if (inputText.getText().toString().length() != 0) {
+        			speakText(inputText.getText().toString());
+        		} else {
+        			speakText("Please tell me what to say.");
+        		}
+			}
+		});
+        
+		mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			public void onCompletion(MediaPlayer mPlayer) {
+				mPlayer.reset();
+			}
+		});
+    }
+    
+    public void speakText(String text) {
+	    try {
+			mPlayer.setDataSource("http://translate.google.com/translate_tts?tl=en&q="+URLEncoder.encode(text));
+		    mPlayer.prepare();		    
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		mPlayer.start();
+    }
+}
