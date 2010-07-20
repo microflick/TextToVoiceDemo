49f6c973ad8ec25862850ad9b504a17c9d576b9d
Zak Ainsworth <zakainsworth@gmail.com>
2010-07-20 01:14:24 -0400

    Fixed indentation.

--- src/com/zakainsworth/TextToVoiceDemo/TextToVoiceDemo.java
+++ src/com/zakainsworth/TextToVoiceDemo/TextToVoiceDemo.java
@@ -13,37 +13,39 @@ public class TextToVoiceDemo extends Activity {
 
 	private Button speakBtn;
 	private EditText inputText;
-    private MediaPlayer mPlayer = new MediaPlayer();
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        inputText = (EditText) findViewById(R.id.inputText);
-        speakBtn = (Button) findViewById(R.id.speakBtn);
-        
-        speakBtn.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-        		if (inputText.getText().toString().length() != 0) {
-        			speakText(inputText.getText().toString());
-        		} else {
-        			speakText("Please tell me what to say.");
-        		}
+	private MediaPlayer mPlayer = new MediaPlayer();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		inputText = (EditText) findViewById(R.id.inputText);
+		speakBtn = (Button) findViewById(R.id.speakBtn);
+
+		speakBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (inputText.getText().toString().length() != 0) {
+					speakText(inputText.getText().toString());
+				} else {
+					speakText("Please tell me what to say.");
+				}
 			}
 		});
-        
+
 		mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			@Override
 			public void onCompletion(MediaPlayer mPlayer) {
 				mPlayer.reset();
 			}
 		});
-    }
-    
-    public void speakText(String text) {
-	    try {
+	}
+
+	public void speakText(String text) {
+		try {
 			mPlayer.setDataSource("http://translate.google.com/translate_tts?tl=en&q="+URLEncoder.encode(text));
-		    mPlayer.prepare();		    
+			mPlayer.prepare();		    
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		} catch (IllegalStateException e) {
@@ -52,5 +54,5 @@ public class TextToVoiceDemo extends Activity {
 			e.printStackTrace();
 		}
 		mPlayer.start();
-    }
+	}
 }
